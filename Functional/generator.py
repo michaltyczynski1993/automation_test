f5ffccb9bae6ffb0d08ba4341a4a876406a31cc3
Michał <m.tyczynski1993@gmail.com>
2022-02-12 14:00:19 +0100

    Changed generator file to make list of all lower case letters to generate name and last name.

--- Functional/generator.py
+++ Functional/generator.py
@@ -5,10 +5,10 @@ USER_LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 PASSWORD_LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y',
                      'z','0','1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M',
                      'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','&','*']
-NAMES = ["Marcin", "Tomasz", "Michal", "Witold", "Piotr", "Zygfryd", "Zygmunt", "Grzegorz", "Maciej", "Karol", "Jakub",
-         "Oskar"]
-LASTNAMES = ["Ostrowicz", "Marcinkiewicz", "Marcinowski", "Otopowicz", "Hala", "Wrona", "Nowak", "Czubak", "Ochman",
-             "Czajka", "Pies", "Kot","Myszka", "Roman", "Kaszuba", "Sala", "Rogala"]
+NAMES = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y',
+                     'z']
+LASTNAMES = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y',
+                     'z']
 
 def user_generator(char_number = 12):
     """generate userName based on site requirements"""
@@ -26,13 +26,19 @@ def password_generator(char_number = 20):
         password += PASSWORD_LETTERS[i]
     return password
 
-def name_generator():
-    """generate random name from list"""
-    name = NAMES[random.randrange(len(NAMES)) - 1]
+def name_generator(min_number = 4, max_number = 10):
+    """generate random name from list char"""
+    name = ""
+    for i in range(min_number, max_number):
+        i = random.randrange(len(NAMES) - 1)
+        name += PASSWORD_LETTERS[i]
     return name
 
-def last_name_generator():
-    """generate random name from list"""
-    last_name = LASTNAMES[random.randrange(len(NAMES)) - 1]
+def last_name_generator(min_number = 3, max_number = 10):
+    """generate random last name from list char"""
+    last_name = ""
+    for i in range(min_number, max_number):
+        i = random.randrange(len(LASTNAMES) - 1)
+        last_name += PASSWORD_LETTERS[i]
     return last_name
 
